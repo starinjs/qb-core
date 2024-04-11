f0a307358b693839823b070fd69c2c11dd7ba177
Adrian <c.count3700@gmail.com>
2024-04-11 17:41:35 +0300

    Latvian retranslate
    Fix some latvian issues

--- locale/lv.lua
+++ locale/lv.lua
@@ -1,21 +1,21 @@
 local Translations = {
     error = {
-        not_online = 'Spēlētājs nav online',
+        not_online = 'Spēlētājs nav tiešsaistē',
         wrong_format = 'Nepareizs formāts',
-        missing_args = 'Ne visi argumenti tika ievadīti (x, y, z)',
+        missing_args = 'Visi argumenti netika ievadīti (x, y, z)',
         missing_args2 = 'Visiem argumentiem ir jābut aizpildītiem!',
         no_access = 'Nav piekļuve šai commandai',
-        company_too_poor = 'Jūsu darba vedējs ir nabadzīgs',
-        item_not_exist = 'Šī lieta nēeksistē',
+        company_too_poor = 'Jūsu darba devējs ir pārāk nabadzīgs',
+        item_not_exist = 'Šīs priekšmets nēeksistē',
         too_heavy = 'Inventārs ir pārāk pilns'
     },
     success = {},
     info = {
-        received_paycheck = 'Jūs esat saņēmuši savu algu par $%{value}',
+        received_paycheck = 'Jūs esat saņēmuši savu algu $%{value}',
         job_info = 'Darbs: %{value} | Pakāpe: %{value2} | Pienākums: %{value3}',
         gang_info = 'Banda: %{value} | Pakāpe: %{value2}',
-        on_duty = 'Jūs tagad esat dienestā!',
-        off_duty = 'Jūs tagad esat atbrīvots no dienesta!'
+        on_duty = 'Jūs tagad strādājat!',
+        off_duty = 'Jūs tagad nestrādājiet!'
     }
 }
 
